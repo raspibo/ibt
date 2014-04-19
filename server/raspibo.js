87a037bb8788068b003a45ede5c8265e15ac1ebf
Davide Alberani <da@erlug.linux.it>
2014-04-20 01:43:50 +0200

    server side

---
+++ server/raspibo.js
@@ -0,0 +1,11 @@
+/** NodeJS RaspiBO Server. */
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8042);
+
+
