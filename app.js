463f304da98431e127b564df03c0c09c33287ce3
Davide Alberani <da@erlug.linux.it>
2014-04-21 21:39:06 +0200

    nodejs app

--- app.js
+++ app.js
@@ -1,13 +1,53 @@
 
 var path = require('path');
+var logger = require('morgan');
 var express = require('express');
 
 var app = express();
 
+app.use(logger());
 app.use(express.static(path.join(__dirname, 'webui')));
 
+function enabledDates(year, month) {
+	month -= 1;
+	var d = new Date(year, month);
+	var validDays = [];
+	var date;
+	var dayOfWeek;
+	var tueCounts = 0;
+	while (d.getMonth() == month) {
+		dayOfWeek = d.getDay();
+		date = d.getDate();
+		if (dayOfWeek == 2) {
+			tueCounts += 1;
+		}
+		if (dayOfWeek == 1 || dayOfWeek == 4 || (dayOfWeek == 2 && (tueCounts == 2 || tueCounts == 4))) {
+			validDays.push(date);
+		}
+		d = new Date(year, month, date+1);
+	}
+	return validDays;
+}
+
+
+app.route('/utils/:util')
+.get(function(req, res, next) {
+	if (req.params.util == 'valid-dates-in-month') {
+		res.json(enabledDates(req.param('year'), req.param('month')));
+	}
+});
+
+app.route('/data*')
+.get(function(req, res, next) {
+	res.send('DATA get ' + req.path);
+}).post(function(req, res, next) {
+	res.send('DATA put');
+}).all(function(req, res, next) {
+	res.send('DATA all');
+});
+
 app.get('/groups*', function(req, res){
-	res.send(req);
+	res.send("XIAO");
 });
 
 app.listen(3000);
