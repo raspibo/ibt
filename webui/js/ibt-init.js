7935d9f2baa2fbc32a57535cf231cf1049eccca7
Davide Alberani <da@erlug.linux.it>
2014-04-25 12:54:30 +0200

    refactoring

--- webui/js/ibt-init.js
+++ webui/js/ibt-init.js
@@ -1,5 +1,56 @@
 
-function log(msg) {
-	console && console.log && console.log(msg);
+var ibt = {};
+
+ibt._noop = function() {};
+
+/** log a message to the console with the given log level. */
+ibt.log = function(msgs, level) {
+	var msg;
+	if (level === undefined) {
+		level = ibt.log.LEVEL_INFO;
+	}
+	if (level < ibt.log.DEBUG_LEVEL || !console) {
+		return;
+	}
+	if (!(msgs instanceof Array)) {
+		msgs = [msgs];
+	}
+	for (var i=0; i < msgs.length; i++) {
+		msg = msgs[i];
+		if (level == ibt.log.LEVEL_DEBUG) {
+			console.debug && console.debug(msg);
+			continue;
+		} else if (level == ibt.log.LEVEL_INFO) {
+			console.info && console.info(msg);
+			continue;
+		} else if (level == ibt.log.LEVEL_WARN) {
+			console.warn && console.warn(msg);
+			continue;
+		} else if (level == ibt.log.LEVEL_ERROR) {
+			console.error && console.error(msg);
+			continue;
+		}
+		console.log && console.log(msg);
+	}
 }
+
+/* Available log levels. */
+ibt.log.LEVEL_DEBUG = 0;
+ibt.log.LEVEL_INFO = 1;
+ibt.log.LEVEL_WARN = 2;
+ibt.log.LEVEL_ERROR = 3;
+
+ibt.log.setLevel = function(level) {
+	ibt.log.DEBUG_LEVEL = level;
+};
+
+// Set the default log level.
+ibt.log.setLevel(ibt.log.LEVEL_WARN);
+ibt.log.setLevel(ibt.log.LEVEL_DEBUG);
+
+// Shortcut functions for logging.
+ibt.debug = function(msg) { ibt.log(msg, ibt.log.LEVEL_DEBUG); };
+ibt.info = function(msg) { ibt.log(msg, ibt.log.LEVEL_INFO); };
+ibt.warn = function(msg) { ibt.log(msg, ibt.log.LEVEL_WARN); };
+ibt.error = function(msg) { ibt.log(msg, ibt.log.LEVEL_ERROR); };
 
